e6b81fe11282e1e7d5e02f296717f361502b9e60
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-02 11:54:51 -0800

    create snippet form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,35 @@
 export default function SnippetCreatePage() {
   return (
-    <div>
-      <h1>Create Snippet Page</h1>
-    </div>
+    <form action="">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12 flex items-center">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12 flex items-center">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
   );
 }
